de6ced13b143718c4b488f751107a5785da41075
Shmliy <42091283+hanakoNya@users.noreply.github.com>
2025-02-18 14:04:09 +0800

    Update si.h

--- app/si.h
+++ app/si.h
@@ -15,4 +15,6 @@ void SI_deinit();
 void Key_FM(KeyboardState kbds) ;
 void HandleUserInput() ;
 void SI4732_Main();
+void setVolume(int volume);
+void SetState(int state);
 #endif /* end of include guard: APP_SI_H */
